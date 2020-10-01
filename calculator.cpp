2b3468ae8665f710d9f8faf90bb7097499ccdbdc
Virag Patel <19it114@charusat.edu.in>
2020-10-01 12:46:17 +0530

    Update calculator.cpp
    Added a function to subtract two numbers.

--- calculator.cpp
+++ calculator.cpp
@@ -5,6 +5,9 @@ using namespace std;
 double add(double num1, double num2) {
   return num1 + num2;
 }
+
+double subtract(double a, double b)
+  return a-b;
 // this is alpha
 int main() {
 
@@ -16,6 +19,7 @@ int main() {
   cin >> num2;
 
   cout << "Addition of first number and second number is: " << add(num1, num2);
+  cout << "Subtraction of first number and second number is: " << subtract(num1, num2);
 
   return 0;
 }
